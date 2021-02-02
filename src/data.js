a1017c8b38bc9a53ce2e76ed0844fc1334526409
AnaliMtzart <analimjack@gmail.com>
2021-02-02 17:15:18 -0600

    version1

--- src/data.js
+++ src/data.js
@@ -1,9 +1,112 @@
-// estas funciones son de ejemplo
+export const setCards = (pokemons, pokemonCard) => {
+  let html = '';
+  pokemons.forEach(pokemon => {
+    html += pokemonCard(pokemon);
+  });
+  return html;
+}
 
-export const example = () => {
-  return 'example';
-};
+export const filterName = (pokemons, name) => {
+  const accordingName = pokemons.filter(pokemon => {
+    if (pokemon.name === name) {
+      return pokemon
+    }
+  });
+  console.log(accordingName);
+  return accordingName;
+}
+export const filterCategory = (pokemons, option,radio) => {
+  const accordinCategory = pokemons.filter(pokemon => {
+    for (const i in pokemon[radio]) {
+      
+      if (pokemon[radio][i] === option) {
+        return pokemon;
+      }
+    }
+  });
+  //console.log(accordinCategory)
+  return accordinCategory;
+}
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const sortData = (pokemons, sortBy, sortOrder) => {
+  //pokemons.sort((a, b) => a.sortBy - b.sortBy);
+  if (sortBy === 'name' && sortOrder === 'nameAsc') {
+    const orderAsc = pokemons.sort((a, b) => {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
+    //console.log(orderAsc);
+    return orderAsc;
+  } else if (sortBy === 'name' && sortOrder === 'nameDesc') {
+    const orderDesc = pokemons.sort((a, b) => {
+      if (a.name < b.name) return 1;
+      if (a.name > b.name) return -1;
+      return 0;
+    });
+    //console.log(orderDesc);
+    return orderDesc;
+    } else if(sortBy === 'num' && sortOrder === 'numDesc'){
+      const numDesc=pokemons.sort((a, b) => b.num - a.num);
+      //console.log(numDesc);
+      return numDesc;
+    }else{
+      const numAsc=pokemons.sort((a, b) => a.num - b.num);
+      //console.log(numAsc);
+      return numAsc;
+    }
+
+}
+
+/* export const pokemonsType=(pokemons)=>{
+  let filtro='type';
+  let types=[];
+  pokemons.filter(pokemon => {
+    for (const i in pokemon.type) {
+      types.push(pokemon.type[i]);
+    }
+  });
+  const dataArr = new Set(types);
+  //return dataArr;
+  console.log(dataArr);
+} */
+export const pokemonsRadio=(pokemons,radio)=>{
+  //console.log('hola');
+  //let radio='type';
+  let filterRadio = pokemons.map (pokemon => pokemon[radio]); 
+  let filterReduce=filterRadio.reduce((actual,siguiente)=>{ 
+    return actual.concat(siguiente);
+  });
+  const filter = new Set(filterReduce);  
+  return filter;
+}
+
+export const setSelect = (selectFilter, selectCategory) => {
+  let html = '<option>Selecciona uno:</option>';
+  selectFilter.forEach(filter => {
+    html += selectCategory(filter);
+  });
+  return html;
+}
+/* export const types = (pokemons) =>{
+  let result = pokemons.map (pokemon => `${pokemon.type}`);
+  console.log(result);
+} */
+
+export const filterCard = (pokemons, num) => {
+  const accordingNum = pokemons.filter(pokemon => {
+    if (pokemon.num === num) {
+      return pokemon
+    }
+  });
+  console.log(accordingNum);
+  return accordingNum;
+}
+
+export const setSingleCards = (pokemons, singlePokemonCard) => {
+  let html = '';
+  pokemons.forEach(pokemon => {
+    html += singlePokemonCard(pokemon);
+  });
+  return html;
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,207 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import { setCards, filterName, sortData, pokemonsRadio, setSelect,filterCategory} from './data.js';
+//import {filterData} from './data.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//console.log(example, data);
 
-console.log(example, data);
+const pokemons = data.pokemon;
+let selectFilter = null;
+let radio=null;
+let accordinCategory=null;
+
+const pokemonCard = (pokemon) => {
+    const component = `
+    <div class="card" id="card">
+    <input class="idPokemon" id="${pokemon.num}" type="hidden" value="${pokemon.num}">
+      <h3 class="styleh3" id=${pokemon.num}>N.°: ${pokemon.num}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong class="strongStyle">${pokemon.name}</strong></h3>
+      <img class="imgpokemon" src="${pokemon.img}" alt="${pokemon.name}">
+      <h3 class="styleh3">Tipo: ${pokemon.type}</h3>
+    </div>`
+    return component;
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const html = setCards(pokemons, pokemonCard);
+    //const cardPokemon =document.querySelector("#cardPokemon");
+    //const cardPokemon = document.getElementById('cardPokemon');
+    cardPokemon.innerHTML = html;
+});
+
+document.querySelector('#searchIcon').addEventListener('click', () => {
+    let search = document.querySelector('#search').value;
+    //console.log('busca: ' + search);
+    let accordingName = filterName(pokemons, search);
+    const html = setCards(accordingName, pokemonCard);
+    //let cardPokemon=document.querySelector("#cardPokemon");
+    cardPokemon.innerHTML = html;
+});
+
+const functCategory = ()=>{
+    let option = document.querySelector('#category').value;
+    console.log('seleccionaste ' + option)
+    accordinCategory=filterCategory(pokemons,option,radio);
+    console.log(accordinCategory) 
+    const html=setCards(accordinCategory,pokemonCard);
+    //let cardPokemon=document.querySelector("#cardPokemon");
+    cardPokemon.innerHTML = html;
+} 
+
+document.querySelector('#category').addEventListener('change',functCategory);
+
+const functOrder = () => {
+    let option = document.querySelector('#order').value;
+    let data=null;
+    if(accordinCategory!=null){
+        data=accordinCategory;
+        console.log(data)
+    }else{
+        data=pokemons;
+    }
+    
+    switch (option) {
+        case 'nameAsc':
+            let nameAsc = sortData(data, 'name', 'nameAsc');
+            const htmlAsc = setCards(nameAsc, pokemonCard);
+            //let cardPokemon=document.querySelector("#cardPokemon");
+            cardPokemon.innerHTML = htmlAsc;
+            break;
+        case 'nameDesc':
+            let nameDesc = sortData(data, 'name', 'nameDesc');
+            const htmlDesc = setCards(nameDesc, pokemonCard);
+            //let cardPokemon=document.querySelector("#cardPokemon");
+            cardPokemon.innerHTML = htmlDesc;
+            break;
+        case 'numAsc':
+            let numAsc = sortData(data, 'num', 'numAsc');
+            const htmlnumAsc = setCards(numAsc, pokemonCard);
+            //let cardPokemon=document.querySelector("#cardPokemon");
+            cardPokemon.innerHTML = htmlnumAsc;
+            break;
+        case 'numDesc':
+            let numDesc = sortData(data, 'num', 'numDesc');
+            const htmlnumDesc = setCards(numDesc, pokemonCard);
+            //let cardPokemon=document.querySelector("#cardPokemon");
+            cardPokemon.innerHTML = htmlnumDesc;
+            break;
+    }
+}
+
+document.querySelector('#order').addEventListener('change', functOrder);
+
+//pokemonsRadio(pokemons);
+//console.log(pokemonsRadio(pokemons,'type'));
+
+let selectCategory = (filter) => {
+    const component =`
+    <option value="${filter}">${filter}</option>`
+    //console.log(component);
+    return component;
+};
+
+const buildSelect=(radio)=>{
+    selectFilter = pokemonsRadio(pokemons,radio);
+    console.log(selectFilter);
+    const html = setSelect(selectFilter, selectCategory);
+    //console.log(selectCategory(selectFilter));
+    const select = document.getElementById('category');
+    select.innerHTML = html;
+}
+
+document.querySelector('#type').addEventListener('click', () => {
+    radio=document.querySelector('#type').value;
+    buildSelect(radio);
+});
+
+document.querySelector('#weaknesses').addEventListener('click', () => {
+    radio=document.querySelector('#weaknesses').value;
+    buildSelect(radio);
+});
+
+document.querySelector('#resistant').addEventListener('click', () => {
+    radio=document.querySelector('#resistant').value;
+    buildSelect(radio);
+});
+
+window.addEventListener("load", function(event) {
+    const cards = document.getElementsByClassName('card');
+    let pokemonsInputs = [];
+    pokemons.forEach(pokemon => {
+        pokemonsInputs.push(document.getElementById(pokemon.num));  
+    });
+    for ( let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener('click', () => {
+            let idPokemon= pokemonsInputs[i].value;
+            localStorage.setItem("idPokemon", idPokemon);
+            location.href ="card.html";
+        });
+    }
+});
+/* primera prueba
+let idCard = document.getElementById("card");
+
+if(idCard){
+    console.log(idCard);
+    document.querySelector('.card').addEventListener('click', () => {
+    console.log("hola");
+});
+}
+
+window.addEventListener("load", function(event) {
+    document.querySelector('#card').addEventListener('click', () => {
+        location.href ="card.html";
+        let idPokemon=document.querySelector('#idPokemon').value;
+        console.log("hola" + idPokemon);
+        localStorage.setItem("idPokemon", idPokemon);
+    });
+  }); */
+
+//TARJETA INFO DE CADA POKEMON AL DARLE CLICL   
+  /*let pokeContainer = document.getElementById("cardPokemon");
+  pokeContainer.addEventListener("click", getAPokemon);
+  
+  function getAPokemon (e) {
+    //console.log('target ' + e.target);
+    console.log('target current' + e.target);
+      //if(e.target !== e.currentTarget){
+      if(e.target){
+          console.log('Hola');
+          let clickedItem = e.target.id;
+          console.log(clickedItem);
+           let getThisPokemon = pokemons.filter ((searchThisPokemon)=> searchThisPokemon.num === clickedItem);
+          console.log(e);
+          document.getElementById("singleCardPokemon").innerHTML = getThisPokemon.map(pokemons).join(''); 
+      }
+  }*/
+
+ /*  //TARJETA INFO DE CADA POKEMON AL DARLE CLICL   
+  let pokeContainer = document.getElementById("cardPokemon");
+  pokeContainer.addEventListener ("click", getAPokemon);
+  
+  function getAPokemon (e) {
+    //console.log('target ' + e.target);
+    console.log('target current' + e.currentTarget);
+      //if(e.target !== e.currentTarget){
+      if(e.currentTarget){
+          console.log('Hola');
+          let clickedItem = e.target.id;
+          console.log(clickedItem);
+          /* let getThisPokemon = pokemons.filter ((searchThisPokemon)=> searchThisPokemon.num === clickedItem);
+          console.log(e);
+          document.getElementById("singleCardPokemon").innerHTML = getThisPokemon.map(pokemons).join(''); 
+      }
+  } */
+
+
+
+
+
+
+
+//console.log(selectCategory(selectFilter));
+
+//let types=filterType(pokemons);
+
+//sortData(pokemons,'name','nameAsc');
+//sortData(pokemons,pokemons.name,"nameAsc");
+
+//filterWeakness(pokemons,'ice'); 
