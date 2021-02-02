2979eb1bef129da9be0875b4784ceb11f2c8031f
MarinaVegaLeyva <marina.vega.condor@gmail.com>
2021-02-02 17:24:08 -0600

    version 1

--- src/data.js
+++ src/data.js
@@ -1,9 +1,88 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const setCards = (pokemons, pokemonCard) => {
+  let html = '';
+  pokemons.forEach(pokemon => {
+    html += pokemonCard(pokemon);
+  });
+  return html;
+}
+export const filterName = (pokemons, name) => {
+  const accordingName = pokemons.filter(pokemon => {
+    if (pokemon.name === name) {
+      return pokemon
+    }
+  });
+  console.log(accordingName);
+  return accordingName;
+}
+export const filterCategory = (pokemons, option,radio) => {
+  const accordinCategory = pokemons.filter(pokemon => {
+    for (const i in pokemon[radio]) {
+      if (pokemon[radio][i] === option) {
+        return pokemon;
+      }
+    }
+  });
+  //console.log(accordinCategory)
+  return accordinCategory;
+}
+export const sortData = (pokemons, sortBy, sortOrder) => {
+  //pokemons.sort((a, b) => a.sortBy - b.sortBy);
+  if (sortBy === 'name' && sortOrder === 'nameAsc') {
+    const orderAsc = pokemons.sort((a, b) => {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
+    //console.log(orderAsc);
+    return orderAsc;
+  } else if (sortBy === 'name' && sortOrder === 'nameDesc') {
+    const orderDesc = pokemons.sort((a, b) => {
+      if (a.name < b.name) return 1;
+      if (a.name > b.name) return -1;
+      return 0;
+    });
+    //console.log(orderDesc);
+    return orderDesc;
+    } else if(sortBy === 'num' && sortOrder === 'numDesc'){
+      const numDesc=pokemons.sort((a, b) => b.num - a.num);
+      //console.log(numDesc);
+      return numDesc;
+    }else{
+      const numAsc=pokemons.sort((a, b) => a.num - b.num);
+      //console.log(numAsc);
+      return numAsc;
+    }
+}
+export const pokemonsRadio=(pokemons,radio)=>{
+  //console.log('hola');
+  //let radio='type';
+  let filterRadio = pokemons.map (pokemon => pokemon[radio]); 
+  let filterReduce=filterRadio.reduce((actual,siguiente)=>{ 
+    return actual.concat(siguiente);
+  });
+  const filter = new Set(filterReduce);  
+  return filter;
+}
+export const setSelect = (selectFilter, selectCategory) => {
+  let html = '<option>Selecciona uno:</option>';
+  selectFilter.forEach(filter => {
+    html += selectCategory(filter);
+  });
+  return html;
+}
+export const filterCard = (pokemons, num) => {
+  const accordingNum = pokemons.filter(pokemon => {
+    if (pokemon.num === num) {
+      return pokemon
+    }
+  });
+  console.log(accordingNum);
+  return accordingNum;
+}
+ export const setSingleCards = (pokemons, singlePokemonCard) => {
+  let html = '';
+  pokemons.forEach(pokemon => {
+    html += singlePokemonCard(pokemon);
+  });
+  return html;
+} 
---
+++ src/mainCard.js
@@ -0,0 +1,32 @@
+import {setSingleCards, filterCard} from './data.js';
+import data from './data/pokemon/pokemon.js';
+const pokemons = data.pokemon;
+const singlePokemonCard = (pokemon) => {
+    const component = `
+    <div id="cardsolo">
+        <div id="idBox">
+            <p class="IdP">N.º${pokemon.num}<br>${pokemon.name}<br>Rarity: ${pokemon['pokemon-rarity']}</p>
+        </div>     
+        <div class="idBoxAbout">
+            <p>${pokemon.about}</p>  
+        </div>
+        <div class="idBoxImg">
+            <img src="${pokemon.img}" alt="${pokemon.name}">
+        </div>
+        <div class="idBoxdescription">
+            <p class="descP">
+                height: ${pokemon.size['height']} <br>
+                weight: ${pokemon.size['weight']} <br>
+                pokemon-rarity: ${pokemon['pokemon-rarity']} <br>
+                type: ${pokemon.type}
+            </p>
+        </div>
+    </div>    `;
+    return component
+}
+window.addEventListener('DOMContentLoaded', () => {
+    let numero = localStorage.getItem("idPokemon");
+    let accordingNum=filterCard(pokemons,numero);
+    const html = setSingleCards(accordingNum, singlePokemonCard);
+    singleCardPokemon.innerHTML = html;
+});
