d135c5a17d58b2224ac840c35e1a7a4ac9eac2bb
MarinaVegaLeyva <marina.vega.condor@gmail.com>
2021-02-08 11:48:42 -0600

    evolutions

--- src/data.js
+++ src/data.js
@@ -5,13 +5,13 @@ export const setCards = (pokemons, pokemonCard) => {
   });
   return html;
 }
+
 export const filterName = (pokemons, name) => {
   const accordingName = pokemons.filter(pokemon => {
     if (pokemon.name === name) {
       return pokemon
     }
   });
-  console.log(accordingName);
   return accordingName;
 }
 export const filterCategory = (pokemons, option,radio) => {
@@ -22,18 +22,16 @@ export const filterCategory = (pokemons, option,radio) => {
       }
     }
   });
-  //console.log(accordinCategory)
   return accordinCategory;
 }
+
 export const sortData = (pokemons, sortBy, sortOrder) => {
-  //pokemons.sort((a, b) => a.sortBy - b.sortBy);
   if (sortBy === 'name' && sortOrder === 'nameAsc') {
     const orderAsc = pokemons.sort((a, b) => {
       if (a.name < b.name) return -1;
       if (a.name > b.name) return 1;
       return 0;
     });
-    //console.log(orderAsc);
     return orderAsc;
   } else if (sortBy === 'name' && sortOrder === 'nameDesc') {
     const orderDesc = pokemons.sort((a, b) => {
@@ -41,21 +39,17 @@ export const sortData = (pokemons, sortBy, sortOrder) => {
       if (a.name > b.name) return -1;
       return 0;
     });
-    //console.log(orderDesc);
     return orderDesc;
     } else if(sortBy === 'num' && sortOrder === 'numDesc'){
       const numDesc=pokemons.sort((a, b) => b.num - a.num);
-      //console.log(numDesc);
       return numDesc;
     }else{
       const numAsc=pokemons.sort((a, b) => a.num - b.num);
-      //console.log(numAsc);
       return numAsc;
     }
 }
+
 export const pokemonsRadio=(pokemons,radio)=>{
-  //console.log('hola');
-  //let radio='type';
   let filterRadio = pokemons.map (pokemon => pokemon[radio]); 
   let filterReduce=filterRadio.reduce((actual,siguiente)=>{ 
     return actual.concat(siguiente);
@@ -63,6 +57,7 @@ export const pokemonsRadio=(pokemons,radio)=>{
   const filter = new Set(filterReduce);  
   return filter;
 }
+
 export const setSelect = (selectFilter, selectCategory) => {
   let html = '<option>Selecciona uno:</option>';
   selectFilter.forEach(filter => {
@@ -70,19 +65,73 @@ export const setSelect = (selectFilter, selectCategory) => {
   });
   return html;
 }
+
 export const filterCard = (pokemons, num) => {
   const accordingNum = pokemons.filter(pokemon => {
     if (pokemon.num === num) {
       return pokemon
     }
   });
-  console.log(accordingNum);
   return accordingNum;
 }
+
  export const setSingleCards = (pokemons, singlePokemonCard) => {
   let html = '';
   pokemons.forEach(pokemon => {
     html += singlePokemonCard(pokemon);
   });
   return html;
-} 
+}
+
+export const evolutions=(pokemons,numero)=>{
+  let evolutions=null;
+  pokemons.forEach(pokemon=>{
+    if(pokemon['num']===numero){
+      if(pokemon.evolution["next-evolution"]){
+        //console.log(pokemon.evolution["next-evolution"])
+        evolutions = pokemon.evolution["next-evolution"];
+        //console.log(evolutions['next-evolution']);
+      }else{
+        evolutions = 0;
+        //console.log("No tiene evolucion");
+      }
+    }
+  }); 
+  return evolutions;
+}  
+
+export const setEvolutions = (evolutionsPokemon, buildEvolution) => {
+  //console.log("set evolutions"+evolutionsPokemon);
+  let html = '';
+  evolutionsPokemon.forEach(evolution => {
+    html += buildEvolution(evolution);
+  });
+  return html;
+}
+
+/* export const preEvolutions=(pokemons,numero)=>{
+  console.log(numero);
+  let evolutions=null;
+  pokemons.forEach(pokemon=>{
+    if(pokemon['num']===numero){
+      if(pokemon.evolution["prev-evolution"]){
+        //console.log(pokemon.evolution["next-evolution"])
+        evolutions = pokemon.evolution["prev-evolution"];
+        //console.log(evolutions['next-evolution']);
+      }else{
+        evolutions = 0;
+        //console.log("No tiene evolucion");
+      }
+    }
+  }); 
+  return evolutions;
+}  */
+
+export const setPreEvolutions = (evolutionsPokemon, buildPreEvolution) => {
+  //console.log("set evolutions"+evolutionsPokemon);
+  let html = '';
+  evolutionsPokemon.forEach(evolution => {
+    html += buildPreEvolution(evolution);
+  });
+  return html;
+}
